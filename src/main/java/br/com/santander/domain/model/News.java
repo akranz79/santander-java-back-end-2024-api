f269e66d4e7c6266269dc8cd53dca1d35c89d873
Alexandre Kranz <ahkranz79@gmail.com>
2024-05-22 13:21:20 -0300

    add models and repository

---
+++ src/main/java/br/com/santander/domain/model/News.java
@@ -0,0 +1,9 @@
+package br.com.santander.domain.model;
+
+import jakarta.persistence.Entity;
+import br.com.santander.domain.model.BaseItem;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+    
+}
